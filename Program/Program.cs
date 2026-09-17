79f5f6b76dc4b40ec81f4fcd0d8feccad3055e78
ALISAKonovalova <alisakonovalova8814@gmail.com>
2026-09-17 14:31:54 +0300

    add line

--- Program/Program.cs
+++ Program/Program.cs
@@ -7,6 +7,7 @@ class Program
         Console.WriteLine("Enter your name:");
         string? name = Console.ReadLine();
         Console.WriteLine($"Hello, {name}");
-        
+         Console.WriteLine("Shawala???");
+
     }
 }
